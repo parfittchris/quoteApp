06711f9eb0989ed062e256c96a2afff78c72260f
Chris Parfitt <parfitt.chris7@gmail.com>
2020-01-27 09:34:30 -0500

    edit scheduler import

--- jobs.py
+++ jobs.py
@@ -1,14 +1,13 @@
-from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 import app
 
 
 
 schedule = BlockingScheduler()
 
-@schedule.scheduled_job('cron', hour='9', minute='30', timezone='America/New_York')
+@schedule.scheduled_job('cron', hour='9', minute='40', timezone='America/New_York')
 def get_quote():
     app.send_message()
 
-# schedule.add_job(app.send_message, trigger='cron', hour='9', minute='10')
 schedule.start()
 
